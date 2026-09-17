2553474c0dc4984a67f20cb34b0c67379c48e70d
Eula <eulfs@proton.me>
2026-09-17 17:25:58 +0200

    2nd commit

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # 9transport
 
-a simple python http webserver meant for transferring files to a 9front machine using hget.
+a simple forth http webserver meant for transferring files to a 9front machine using hget.
+
+### Issues:
+the 1991.fs module is pretty laggy sometimes, so it isn't expected to work _all_ of the time.
+
+![9front image](Example/glenda.jpg "Title")
--- main.f
+++ main.f
@@ -1,15 +1,21 @@
 \ 9Transport loading modules.
 include 1991.fs
 
+: GET-NUMBER ( -- n )
+	." Return a port number> " CR
+	PAD 32 ACCEPT
+	PAD SWAP
+	S>NUMBER? 0= ABORT" NOT A NUMBER."
+	DROP ;
+
 s" ip a" system
 
 CR CR
 
-\ Changes made here will change how the program works, for example, changing the port "8080" to "80"
 
-s" ." set-public-path
-8080 s" You are running 9transport on port " type . ." currently. "
+s" Example" set-public-path
+GET-NUMBER DUP s" You are running 9transport on port " type . ." currently. "
 CR CR
 s" If you wish to change this, change it in your ``main.f``" type CR
 
-8080 1991:
+1991:
